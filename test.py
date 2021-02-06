f11fd9dc2434ad52bc21d3a1e3921e23e05baefa
Henry Haefliger <54628404+hnhaefliger@users.noreply.github.com>
2021-02-06 19:32:46 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,3 @@
 import autowebdriver
 
-print(getDriverURL('Google Chrome', '86.0.4240.111', 'Darwin', '64'))
-print(getDriverURL('Opera', '72.0', 'Darwin', '64'))
-print(getDriverURL('Edge', '75.0.139.20', 'Windows', '64'))
-print(getDriverURL('Internet Explorer', '75.0.139.20', 'Windows', '64'))
-print(getDriverURL('Firefox', '75.0.139.20', 'Darwin', '64'))
-
 print(autowebdriver.getdriver())
